37c3cb57e296ebd26685d739a8875fde1874dce6
hans <hans.git@furg.br>
2023-04-30 16:36:13 -0300

    first second commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hans/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hans/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/hans/catkin_ws/devel;/opt/ros/noetic' != '' else []
