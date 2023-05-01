7d549c80ab7488601285f6c02aa6004eef5363df
hans <hans.git@furg.br>
2023-05-01 16:58:59 -0300

    Moving to the point

--- README.md
+++ README.md
@@ -16,3 +16,37 @@ roslaunch stage_controller launcher.launch
 catkin_make
 source devel/setup.zsh
 ```
+f -> stop camera
+
+
+odom -> nav_msgs/Odometry
+
+
+pose: 
+  pose: 
+    position: 
+      x: 0.45000014603139094
+      y: 2.6333335453851503
+      z: 0.0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.6997160753466033
+      w: 0.7144210340559316
+
+
+
+rosmsg show sensor_msgs/LaserScan 
+std_msgs/Header header
+  uint32 seq
+  time stamp
+  string frame_id
+float32 angle_min
+float32 angle_max
+float32 angle_increment
+float32 time_increment
+float32 scan_time
+float32 range_min
+float32 range_max
+float32[] ranges
+float32[] intensities
--- src/stage_controller/scripts/stage_controller.py
+++ src/stage_controller/scripts/stage_controller.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import *
 from nav_msgs.msg import *
 import random
 import math
+from tf.transformations import euler_from_quaternion
 
 laser = LaserScan()
 odometry = Odometry()
@@ -24,8 +25,8 @@ if __name__ == "__main__":
 
 	rospy.Subscriber("/base_scan", LaserScan, laser_callback)
 
-	target_x = 8.0
-	target_y = 13.0
+	target_x = 0.0
+	target_y = 8.0
 
 	min_distance = 0.1
 
@@ -40,12 +41,27 @@ if __name__ == "__main__":
 		# Verifica se chegou ao alvo
 		distance = math.sqrt((x-target_x)**2 + (y-target_y)**2)
 		if (distance > min_distance):
-			rospy.loginfo("Where i am: X: %s, Y: %s", x, y)
-
-			velocity.linear.x = random.uniform(0.0, 0.5)
-			pub.publish(velocity)
 
 			# FACA O SEU CODIGO AQUI
+			orientation_q = odometry.pose.pose.orientation
+			orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+			(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+			angle_to_target = math.atan2(target_y-y, target_x-x)
+			angle_diff = angle_to_target - yaw
+			rospy.loginfo("Where i am: X: %s, Y: %s, Angle_target: %s, Angle_diff %s", x, y,angle_to_target,angle_diff)
+			
+			if (len(laser.ranges) > 0):
+				if (min(laser.ranges[45*4:225*4]) > 0.25):
+					velocity.linear.x = 0.5 * distance
+					velocity.angular.z = 0.5 * angle_diff
+					
+				else:
+					velocity.linear.x = 0.0
+					velocity.angular.z = 0.5 * (angle_diff/angle_diff)
+				
+				pub.publish(velocity)
+
+
 			
 		else:
 			velocity.linear.x = 0.0
